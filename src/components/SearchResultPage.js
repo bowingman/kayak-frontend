4926054fef49cfa334425e4eb45f18a0027ce231
bowingman <ngmaster.sag@gmail.com>
2017-12-02 23:58:00 -0800

    Update SearchResultPage.js

--- src/components/SearchResultPage.js
+++ src/components/SearchResultPage.js
@@ -1,21 +1,46 @@
 import React from 'react'
 import img1 from '.././images/AccountIcon.png'
 import kayak from '.././images/KAYAK.png'
+import stars5 from '.././images/5stars.png'
+import stars4 from '.././images/4stars.png'
+import stars3 from '.././images/3stars.png'
+import stars2 from '.././images/2stars.png'
+import footer from '.././images/footer.png'
+import rightpaneimage1 from '.././images/rightPaneImage1.png'
+import rightpaneimage2 from '.././images/rightPaneImage2.png'
+import hoteldesc from '.././images/hoteldesc.png'
+import * as API from '../api/API';
 
 class SearchResultPage extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            searchResults: []
+        };
     }
+
+
+
+    componentDidMount() {
+        API.getHotelSearchResults()
+            .then((data) => {
+                console.log(JSON.stringify(data.data[0]));
+                this.setState({
+                    searchResults: data.data
+                });
+            });
+    };
 
     render() {
         return (
-            <div className="container-fluid">
+            <div className="container-fluid searchListPage" style={{backgroundColor:"#e4e5ea", paddingBottom:"25px"}}>
                 <nav className="navbar navbar-inverse">
                     <div className="container-fluid">
                         <div className="navbar-header">
-                            <a className="navbar-brand" href="/"><h1>Kayak</h1></a>
+                            <a className="navbar-brand" href="/"><img src={kayak} width={140} style={{paddingTop: 15}}/></a>
                         </div>
                         <ul className="nav">
+
                             <li className="active"><a href="/"><h2>Home</h2></a></li>
                             <li><a href="/"><h2>Hotels</h2></a></li>
                             <li><a href="/"><h2>Flights</h2></a></li>
@@ -25,13 +50,11 @@ class SearchResultPage extends React.Component {
                     </div>
                 </nav>
 
-                <div className="row">
-                    <div className="col-md-2" style={{paddingTop: 15}}>
-                        <div className="nav-side-menu col-md-2" style={{textAlign: 'Center'}}>
-                            <a className="navbar-brand" href="/"><img src={kayak} width={140} style={{paddingTop: 15}}/></a>
-                            <br/><br/><br/><br/>
+                <div className="row" style={{paddingBottom:'50px'}}>
+                    <div className="col-md-3" style={{paddingTop: 15}}>
+                        <div className="nav-side-menu col-md-8" style={{textAlign: 'Center', top:'4px', backgroundColor:'white', marginLeft:'100px', height:'500px'}}>
                             <br/><br/>
-                            <button type="button" className="btn btn-info" data-toggle="collapse" data-target="#demo">
+                            <button type="button" className="btn btn-info filterbuttons" data-toggle="collapse" data-target="#demo">
                                 <h3>Select Stars</h3>
                             </button>
                             <br/><br/>
@@ -45,7 +68,7 @@ class SearchResultPage extends React.Component {
                                 </ul>
                             </div>
                             <br/><br/>
-                            <button type="button" className="btn btn-info" data-toggle="collapse" data-target="#demo1">
+                            <button type="button" className="btn btn-info filterbuttons" data-toggle="collapse" data-target="#demo1">
                                 <h3>Select Ratings</h3>
                             </button>
                             <br/><br/>
@@ -59,7 +82,7 @@ class SearchResultPage extends React.Component {
                                 </ul>
                             </div>
                             <br/><br/>
-                            <button type="button" className="btn btn-info" data-toggle="collapse" data-target="#demo2">
+                            <button type="button" className="btn btn-info filterbuttons" data-toggle="collapse" data-target="#demo2">
                                 <h3>Select Stars</h3>
                             </button>
                             <br/><br/>
@@ -73,7 +96,7 @@ class SearchResultPage extends React.Component {
                                 </ul>
                             </div>
                             <br/><br/>
-                            <button type="button" className="btn btn-info" data-toggle="collapse" data-target="#demo3">
+                            <button type="button" className="btn btn-info filterbuttons" data-toggle="collapse" data-target="#demo3">
                                 <h3>Select Stars</h3>
                             </button>
                             <br/><br/>
@@ -89,37 +112,64 @@ class SearchResultPage extends React.Component {
                             <br/><br/>
                         </div>
                     </div>
-                    <div className="col-md-10">
-                        <h1>Search Results</h1>
-                        <div className="well well-lg" style={{padding: "10"}}>
-                            <div className="row">
-                                <div className="col-md-4"><img src={img1} style={{width: "50%"}}/></div>
-                                <div className="col-md-4">
-                                    <div className="row">
-                                        <div className="col-md-12"><h4 className="list-group-item-heading">Name</h4>
-                                        </div>
+                    <div className="col-md-6 SearchListMainContainer">
+                        {this.state.searchResults.map((task, i) =>
+                            <div className="SearchListItemMainContainer">
+                                <span className="SearchListItemImage">
+                                    <img src={task.hotel_image}/>
+                                </span>
+                                <span className="SearchListItemMainContent">
+                                    <div className="SearchListItemTitle">{task.hotel_name}</div>
+                                    <div className="SearchListItemAdd">{task.hotel_address}</div>
+                                    <div className="SearchListItemStars">
+
+                                        {
+                                        task.hotel_stars == "5" ? (
+
+                                            <img className="recentImage" src={stars5} alt={'logo5'}/>
+                                        ) : (
+                                            task.hotel_stars == "4" ? (
+                                                <img className="recentImage" src={stars4} alt={'logo4'}/>
+                                            ) : (
+                                                task.hotel_stars == "3" ? (
+
+                                                    <img className="recentImage" src={stars3} alt={'logo3'}/>
+                                                ) : (
+                                                    task.hotel_stars == "2" ? (
+
+                                                        <img className="recentImage" src={stars2} alt={'logo2'}/>
+                                                    ) : (
+                                                        <img className="recentImage" src={stars2} alt={'logo2'}/>
+                                                    )
+                                                )
+                                            )
+                                        )
+                                    }</div>
+                                    <div className="SearchListItemRatings">{task.hotel_ratings}</div>
+                                    <div className="SearchListItemDesc">{task.hotel_description}</div>
+                                    <div className="SearchListItem">
+                                        <img className="hoteldesc" src={hoteldesc} alt={'hoteldesc'}/>
                                     </div>
-                                    <div className="row" style={{padding: "20"}}>
-                                        <div className="col-md-6"><p className="list-group-item-text">Location</p>
-                                        </div>
-                                        <div className="col-md-6"><p className="list-group-item-text">Description
-                                            and Rating</p></div>
-                                    </div>
-                                </div>
-                                <div className="col-md-4">
-                                    <div className="row">
-                                        <div className="col-md-12">
-                                            Contact Number
-                                        </div>
-                                    </div>
-                                    <div className="row" style={{padding: "40"}}>
-                                        <div className="col-md-12">
-                                            <button>Book Now</button>
-                                        </div>
-                                    </div>
-                                </div>
+                                </span>
+                                <span className="SearchListItemPriceContent">
+                                    <div className="SearchListItemPrice">$ {task.hotel_stars}</div>
+                                    <button type="button" className="btn btn-info Viewdeal" >View Deal</button>
+                                </span>
+                            </div>
+                        )}
+
+
+                    </div>
+                    <div className="col-md-3">
+                        <div className="SearchListPageRightContainer">
+                            <div className="SearchListPageRightContainer1">
+                                <img className="rightpaneimage1" src={rightpaneimage1} alt={'image1'}/>
+                            </div>
+                            <div className="SearchListPageRightContainer2">
+                                <img className="rightpaneimage2" src={rightpaneimage2} alt={'image2'}/>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
