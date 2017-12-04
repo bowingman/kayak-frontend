f012310da534d42e37b346bfdd24f7b811841a03
bowingman <ngmaster.sag@gmail.com>
2017-12-03 16:12:18 -0800

    added api calls for selectedhotel_Details get/post

--- src/api/API.js
+++ src/api/API.js
@@ -311,5 +311,44 @@ export const updateHotels = (payload) =>
             return error;
         });
 
+//to get details of hotel being booked
+export const postSelectedHotelToBook = (payload) =>
+    fetch(`${api}/hotels/get_selected_hotel`, {
+        method: 'POST',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        credentials:'include',
+        body: JSON.stringify(payload)
+    }).then(res => {
+        console.log("hotels/search_hotels: "+res);
+        return res.json();
+    })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+
+export const getSelectedHotelToBook = () =>
+    fetch(`${api}/hotels/get_selected_hotel`, {
+        method: 'GET',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        credentials:'include'
+    }).then(res => {
+        console.log(res);
+        return res.json();
+    })
+        .catch(error => {
+            console.log("This is error"+error);
+            return error;
+        });
+
+
+
+
 
 
